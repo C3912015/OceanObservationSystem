1d888b45f7463fd0af2de47a960cb23e9f9827e0
megsum <msumner@ualberta.ca>
2015-10-28 14:38:51 -0600

    Can now insert data

--- Subscribe/subscribe.php
+++ Subscribe/subscribe.php
@@ -11,33 +11,39 @@ and is able to add or remove subscriptions to sensors.
 			include("PHPconnectionDB.php");
 			//establish connection
 			$conn = connect();
+			
+			//sql add values into sensors
+			$sql1 = 'INSERT INTO sensors VALUES (122)'; 
 
-			//sql command
-			$sql = 'SELECT * from sensors';
+			//sql collect all values from sensors
+			$sql2 = 'SELECT * FROM sensors';
 
 			//Prepare sql using conn and returns the statement identifier
-			$stid = oci_parse($conn, $sql);
+			$stid1 = oci_parse($conn, $sql1);
+			$stid2 = oci_parse($conn, $sql2);
 
 			//Execute a statement returned from oci_parse()
-			$res = oci_execute($stid);
-
+			$res1 = oci_execute($stid1);
+			$res2 = oci_execute($stid2);
 			//if error, retrieve the error using the oci_error() function & output an error
-			if (!$res) {
-			$err = oci_error($stid);
-			echo htmlentities($err['message']);
+			if (!$res1) {
+				$err = oci_error($stid1);
+				echo htmlentities($err['message']);
 			} else { echo 'Rows Extracted <br/>'; }
 
 
 			//Display results
-			while (($row = oci_fetch_array($stid, OCI_ASSOC))){
-				foreach($row as $item){
-					echo $item.'&nbsp;';
-				}
-				echo '<br/>';
+		   while ($row = oci_fetch_array($stid2, OCI_ASSOC)) {
+		   	
+			foreach ($row as $item) {
+				echo $item.'&nbsp;';
 			}
+			echo '<br/>';
+		   }
 
 			// Free the statement identifier when closing the connection
-			oci_free_statement($stid);
+			oci_free_statement($stid1);
+			oci_free_statement($stid2);
 			oci_close($conn);
 		?>
 	</body>
