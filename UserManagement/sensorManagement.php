7a578efc43259ebb7258a186fd848da4174a8d12
LeahGabrielle <olexson@ualberta.ca>
2015-10-31 12:15:36 -0600

    new files for sensorManagement

---
+++ UserManagement/sensorManagement.php
@@ -0,0 +1,21 @@
+<!-- Add/Remove Sensor -->
+
+<html>
+  <body>
+    <?php
+       //connection function
+       include("PHPconnectionDB.php")
+
+       //establish connection
+       $conn = connect()
+
+       //get sensor_id
+
+       //get button info
+
+       //if delete, remove sensor
+
+       //if add, new form for info, go to another page
+    ?>
+  </body>
+</html>
