0e04186ad249a56935819074688673406a097ba0
LeahGabrielle <olexson@ualberta.ca>
2015-11-03 15:04:39 -0700

    can now add a sensor, but no error handling

--- UserManagement/sensorManagement.php
+++ UserManagement/sensorManagement.php
@@ -1,5 +1,4 @@
 <!-- Add/Remove Sensor -->
-<!-- add trims -->
 
 <html>
   <body>
@@ -27,20 +26,42 @@
              {
                 $count++;
              }
+             //sensor_id exists, so remove it
              if($count > 0){
                 $sqlDEL = "DELETE FROM sensors 
 			WHERE sensor_id={$sensor_ID}";
                 $rmSensor = oci_parse($conn, $sqlDEL);
                 $res = oci_execute($rmSensor);
                 echo 'Sensor removed';
+             //sensor doesn't exist
              }else{echo "Unable to remove nonexistant sensor.";}
+          //no input given
           oci_free_statement($sensorExist);
           } else { echo "No sensor given"; }
        }
 
        //Add the sensor
+       //check for null values ??
        if($_POST["AddSensor"]){
-          echo('yo');
+          //get values and check they are not null ??
+         if($_POST['ASensorID'] != NULL){ $sensor_ID = $_POST['ASensorID']; 
+         } else { 
+            echo 'No sensor ID given'; 
+            echo '<p><a href="http://consort.cs.ualberta.ca/~olexson/OceanObservationSystem/UserManagement/sensorManagement.html">Go Back</a>';
+            exit;
+         }
+         //if(isset($_POST['sLocation'])){}
+         //if(isset($_POST['sType'])){}
+         //if(isset($_POST['sDesc'])){}
+         $location = $_POST['sLocation'];
+         $sensorType = $_POST['sType'];
+         $sensorDesc = $_POST['sDesc'];
+
+         $sqlADD = "INSERT INTO sensors VALUES ({$sensor_ID},
+         '{$location}', '{$sensorType}', '{$sensorDesc}')";
+         echo $sqlADD;
+         $addSensor = oci_parse($conn, $sqlADD);
+         $res = oci_execute($addSensor);
        }
        echo
        '<p><a href="http://consort.cs.ualberta.ca/~olexson/OceanObservationSystem/UserManagement/sensorManagement.html">Go
