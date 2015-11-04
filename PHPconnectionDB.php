325f04c0ca4cf6129dccefef4f1f14f677e621a1
LeahGabrielle <olexson@ualberta.ca>
2015-11-04 14:32:29 -0700

    Update PHPconnectionDB.php

--- PHPconnectionDB.php
+++ PHPconnectionDB.php
@@ -1,6 +1,6 @@
 <?php
 function connect(){
-	$conn = oci_connect('username', 'password');
+	$conn = oci_connect('olexson', 'yohatdog4');
 	if (!$conn) {
 		$e = oci_error();
 		trigger_error(htmlentities($e['message'], ENT_QUOTES), E_USER_ERROR);
