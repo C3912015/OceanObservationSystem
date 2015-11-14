122510a98f7864fe7f9179d2e09dcad93106d944
LeahGabrielle <olexson@ualberta.ca>
2015-11-14 13:42:17 -0700

    added test comments to upload

--- Uploading/upload.php
+++ Uploading/upload.php
@@ -6,14 +6,20 @@ Curator can:
   -upload scalar data in batches by csv file
   -upload image/audio
 -->
+<!-- http://www.htmlgoodies.com/beyond/php/article.php/3877766/Web-Developer-How-To-Upload-Images-Using-PHP.htm -->
 
 <?php session_start(); ?>
 <html>
   <body>
     <!-- If scalar data -->
+    <!-- scalar_data (id, sensor_id, date_created, value) -->
+	<buton> Hello </button>
+
+
+    <!-- If image .jpg -->
+    <!-- images (image_id, sensor_id, date_created , recorded_data, thumbnail, description) -->
     
-    <!-- If image -->
-    
-    <!-- If audio -->
+    <!-- If audio .wav-->
+    <!-- audio_recordings (recording_id, sensor_id, date_created, length, recorded_data, description) -->
   </body>
 </html>
