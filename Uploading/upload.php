696c091e01808a8aab588863421c1312822d6f43
LeahGabrielle <olexson@ualberta.ca>
2015-11-14 13:18:51 -0700

    Update upload.php

--- Uploading/upload.php
+++ Uploading/upload.php
@@ -1,3 +1,12 @@
+<!--This module will be used by data curators to upload images, 
+audio recordings, and scalar measurements in batches,
+after possible processing and annotations in the description field. 
+Upload all the data stored in the user's local file system to the database. 
+Curator can: 
+  -upload scalar data in batches by csv file
+  -upload image/audio
+-->
+
 <?php session_start(); ?>
 <html>
   <body>
